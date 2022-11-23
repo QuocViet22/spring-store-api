18060622ae0f06edd711d4bef457901cd3b8e32b
unknown <your name>
2022-11-23 17:55:23 +0700

    update AccountInforResponse 23/11/2022

--- src/main/java/com/spring/store/api/payload/request/UpdateAccountRequest.java
+++ src/main/java/com/spring/store/api/payload/request/UpdateAccountRequest.java
@@ -1,14 +1,9 @@
 package com.spring.store.api.payload.request;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Set;
 
 import javax.validation.constraints.*;
 
-@Getter
-@Setter
 public class UpdateAccountRequest {
     @NotBlank
     @Size(min = 3, max = 20)
@@ -19,5 +14,61 @@ public class UpdateAccountRequest {
     private Set<String> role;
     private String setModifiedBy;
     private String getSetModifiedDate;
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getSetModifiedBy() {
+        return setModifiedBy;
+    }
+
+    public void setSetModifiedBy(String setModifiedBy) {
+        this.setModifiedBy = setModifiedBy;
+    }
+
+    public String getGetSetModifiedDate() {
+        return getSetModifiedDate;
+    }
+
+    public void setGetSetModifiedDate(String getSetModifiedDate) {
+        this.getSetModifiedDate = getSetModifiedDate;
+    }
 }
 
