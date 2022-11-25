4f1ba32357a3cf45778b79ac5b4b9dbc1c0c04d1
unknown <your name>
2022-11-25 23:25:21 +0700

    fix datatype Image

--- src/main/java/com/spring/store/api/models/Image.java
+++ src/main/java/com/spring/store/api/models/Image.java
@@ -17,7 +17,6 @@ public class Image extends BaseModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
     @Column(name = "link")
     private String link;
 
---
+++ src/main/java/com/spring/store/api/models/Revenue.java
@@ -0,0 +1,5 @@
+package com.spring.store.api.models;
+
+public class Revenue {
+
+}
