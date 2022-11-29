afc2660650ffa3f55488babf0268ef2f80a11430
unknown <your name>
2022-11-29 15:07:39 +0700

    29/11/2022 update payload request

--- src/main/java/com/spring/store/api/payload/request/RevenueByDateRequest.java
+++ src/main/java/com/spring/store/api/payload/request/RevenueByDateRequest.java
@@ -3,8 +3,11 @@ package com.spring.store.api.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class RevenueByDateRequest {
+    @NotBlank
     private String modifiedDate;
 }
--- src/main/java/com/spring/store/api/payload/request/RevenueByMonthRequest.java
+++ src/main/java/com/spring/store/api/payload/request/RevenueByMonthRequest.java
@@ -3,8 +3,11 @@ package com.spring.store.api.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class RevenueByMonthRequest {
+    @NotBlank
     private int year;
 }
