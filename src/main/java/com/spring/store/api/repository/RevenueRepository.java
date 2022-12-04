accb37c9abddbf8c68c9e1242e5536e0c3053333
unknown <your name>
2022-12-04 08:29:23 +0700

    04/12/2022 update RevenueRepository

--- src/main/java/com/spring/store/api/repository/RevenueRepository.java
+++ src/main/java/com/spring/store/api/repository/RevenueRepository.java
@@ -15,29 +15,29 @@ public interface RevenueRepository extends JpaRepository<Order, Long> {
 
     //    revenue by date
     @Query(value =
-            "select o.modified_date as modifiedDate, SUM(CAST(o.total_price as int)) as totalPrice, o.status as status, l.product_id as productId, SUM(CAST(l.amount as int)) as productAmount\n" +
+            "select o.modified_date as modifiedDate, l.total as totalPrice, o.status as status, l.product_id as productId, SUM(CAST(l.amount as int)) as productAmount\n" +
                     "from orders o join line_item_orders l on o.id = l.order_id\n" +
                     "where o.modified_date = ?1 and o.status = '3'\n" +
-                    "group by o.modified_date, o.status, l.product_id\n" +
-                    "order by totalPrice desc;",
+                    "group by o.modified_date, o.status, l.product_id, l.total\n" +
+                    "order by totalPrice desc;\n",
             nativeQuery = true)
     public List<IRevenueByDateResponse> viewRevenueByDate(String modifiedDate);
 
     //    revenue by month
     @Query(value =
-            "select EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) as month, SUM(CAST(o.total_price as int)) as totalPrice, l.product_id as productId, SUM(CAST(l.amount as int)) as amountProduct\n" +
+            "select EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) as month, l.total as totalPrice, l.product_id as productId, SUM(CAST(l.amount as int)) as amountProduct\n" +
                     "from orders o join line_item_orders l on o.id = l.order_id\n" +
                     "where o.status = '3' and EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) = ?1 and EXTRACT(year FROM TO_DATE(modified_date,'dd/mm/yyyy')) = ?2\n" +
-                    "group by EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')), l.product_id;",
+                    "group by EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')), l.product_id, l.total;",
             nativeQuery = true)
-    public List<IRevenueByMonthResponse> viewRevenueByMonth(int month, int year);
+    public List<IRevenueByMonthResponse>    viewRevenueByMonth(int month, int year);
 
     //    top 5 best seller of month
     @Query(value =
-            "select EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) as month, SUM(CAST(o.total_price as int)) as totalPrice, l.product_id as productId, SUM(CAST(l.amount as int)) as amountProduct\n" +
+            "select EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) as month, l.total as totalPrice, l.product_id as productId, SUM(CAST(l.amount as int)) as amountProduct\n" +
                     "from orders o join line_item_orders l on o.id = l.order_id\n" +
                     "where EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')) = ?1 and EXTRACT(year FROM TO_DATE(modified_date,'dd/mm/yyyy')) = ?2 and o.status = '3'\n" +
-                    "group by EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')), l.product_id\n" +
+                    "group by EXTRACT(MONTH FROM TO_DATE(modified_date,'dd/mm/yyyy')), l.product_id, l.total\n" +
                     "order by amountProduct desc\n" +
                     "limit 5;",
             nativeQuery = true)
@@ -45,10 +45,10 @@ public interface RevenueRepository extends JpaRepository<Order, Long> {
 
     //    revenue by month
     @Query(value =
-            "select EXTRACT(month FROM TO_DATE(o.modified_date,'dd/mm/yyyy')) as month, SUM(CAST(o.total_price as int)) as totalPrice\n" +
+            "select EXTRACT(month FROM TO_DATE(o.modified_date,'dd/mm/yyyy')) as month, o.total_price as totalPrice\n" +
                     "from orders o join line_item_orders l on o.id = l.order_id\n" +
                     "where EXTRACT(year FROM TO_DATE(modified_date,'dd/mm/yyyy')) = ?1 and o.status = '3'\n" +
-                    "group by EXTRACT(month FROM TO_DATE(o.modified_date,'dd/mm/yyyy'));\n",
+                    "group by EXTRACT(month FROM TO_DATE(o.modified_date,'dd/mm/yyyy')), o.total_price;",
             nativeQuery = true)
     public List<IRevenuePerMonthResponse> viewRevenuePerMonth(int year);
 
